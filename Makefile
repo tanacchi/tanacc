ec88d083b741d1caba07d191398bbbb2e49a51d8
tanacchi <q111026d@mail.kyutech.jp>
2019-06-11 23:28:34 +0900

    Add test script

--- Makefile
+++ Makefile
@@ -5,5 +5,8 @@ TARGET   = tanacc
 $(TARGET): compiler.c
 	$(CC) $(CC_FLAGS) -o $(TARGET) compiler.c
 
+test:
+	./test.bash
+
 clean:
 	-@rm tanacc *.o *.s
---
+++ test.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#!/bin/bash
+try() {
+  expected="$1"
+  input="$2"
+
+  ./tanacc "$input" > tmp.s
+  gcc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$expected expected, but got $actual"
+    exit 1
+  fi
+
+  rm tmp.s tmp
+}
+
+try 0 0
+try 42 42
+
+echo OK
