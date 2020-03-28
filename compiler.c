994c77f97f5814c9162374a77e207b921f405bc2
tanacchi <q111026d@mail.kyutech.jp>
2020-03-29 03:06:44 +0900

    [Step 3] Add tokenizer

--- compiler.c
+++ compiler.c
@@ -1,37 +1,137 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+typedef enum
+{
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token
+{
+  TokenKind kind;
+  Token* next;
+  int val;
+  char* str;
+};
+
+Token* token;
+
+void error(char* fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+bool consume(char op)
+{
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
+}
+
+void expect(char op)
+{
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("It is not '%c'", op);
+  token = token->next;
+}
+
+int expect_number()
+{
+  if (token->kind != TK_NUM)
+    error("It is'nt a number");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof()
+{
+  return token->kind == TK_EOF;
+}
+
+Token* new_token(TokenKind kind, Token* cur, char* str)
+{
+  Token* tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token* tokenize(char* p)
+{
+  Token head;
+  head.next = NULL;
+  Token* cur = &head;
+
+  while (*p)
+  {
+    if (isspace(*p))
+    {
+      p++;
+      continue;
+    }
+    else if (*p == '+' || *p == '-')
+    {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+    else if (isdigit(*p))
+    {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+    else
+    {
+      error("Clouldn't tokenize.");
+    }
+  }
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char** argv)
 {
   if (argc != 2)
   {
-    printf("Please set a number on second argument.\n");
+    printf("Please set source string on second argument.\n");
     exit(1);
   }
 
-  char* p = argv[1];
+  token = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("\tmov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p)
+  printf("\tmov rax, %d\n", expect_number());
+
+  while (!at_eof())
   {
-    if (*p == '+')
+    if (consume('+'))
     {
-      p++;
-      printf("\tadd rax, %ld\n", strtol(p, &p, 10));
-      continue;
+      printf("\tadd rax, %d\n", expect_number());
     }
-    if (*p == '-')
+    else
     {
-      p++;
-      printf("\tsub rax, %ld\n", strtol(p, &p, 10));
-      continue;
+      expect('-');
+      printf("\tsub rax, %d\n", expect_number());
     }
-
-    fprintf(stderr, "unexpected character was found: '%c'\n", *p);
   }
   printf("\tret\n");
 
