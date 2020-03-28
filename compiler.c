02c017d726de3c1f18404e24a1f3c32be9cddd67
tanacchi <q111026d@mail.kyutech.jp>
2020-03-29 01:16:09 +0900

    Add arithmetic calculation function

--- compiler.c
+++ compiler.c
@@ -9,10 +9,30 @@ int main(int argc, char** argv)
     exit(1);
   }
 
+  char* p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("\tmov rax, %d\n", atoi(argv[1]));
+  printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p)
+  {
+    if (*p == '+')
+    {
+      p++;
+      printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+    if (*p == '-')
+    {
+      p++;
+      printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "unexpected character was found: '%c'\n", *p);
+  }
   printf("\tret\n");
 
   return 0;
